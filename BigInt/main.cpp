d4a2175393b78184e008093f047b97f9c53a027f
di <eglet91@gmail.com>
2018-05-25 17:48:24 +0300

    Update main.cpp

--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -1,26 +1,39 @@
 #include "BigInteger.cpp"
 #include <iostream>
 #include <limits>
+#include <climits>
 
 int main() {
 
+    cout << LLONG_MAX << endl;
+    for (long long i = 1e9 - 100; i <= 1e9; ++i) {
+        BigInteger<char, 8> a(i);
+        if (!a.check())  {
+            cout << "T R O U B L E \n" << i;
+            break;
+        } else {
+            cout << i <<" passed \n";
+        }
+    }
 
-   BigInteger<int, 5> b, c;
+    bool t = true;
 
-    std::cin >> b;
-    std::cin >> c;
+    for (long long i = 1e11 ; i < 1e11 + 500; ++i) {
+        for (long long j = 1e6; j < 1e6 + 500; ++j) {
+            BigInteger <char, 8> a(i), b(j);
+            a *= b;
+            long long ans = i * j;
+            if (to_string(ans) == a.toString()) {
+                cout << a <<" passed\n";
+            } else {
+                t = false;
+                cout <<"T R O U B L E  " << i <<" * " << j << "\n";
+                break;
+            }
+        }
+        if (!t) break;
+    }
 
-    std::cout << "Input :\n";
-    std::cout << b << "\n";
-    std::cout << c << " \n";
-
-    BigInteger<int, 5> a = b + c;
-    std::cout << "b + c = " << a << "\n";
-
-    BigInteger<int, 5> d = b - c;
-    std::cout << "b - c = " << d << "\n";
-
-    std::cout << std::numeric_limits<long long>::max();
 
 
     return 0;
