ad3dbb123ef1e318af72ed8ce222a30fed2d493e
di <eglet91@gmail.com>
2018-05-25 17:47:49 +0300

    Update BigInteger.cpp

--- BigInt/BigInteger.cpp
+++ BigInt/BigInteger.cpp
@@ -1,48 +1,89 @@
+/*  калькулятор : http://itpride.net/useful/ochen-prodvinutyj-onlajn-kalkulyator.html
+ * перевод сс: http://calculatori.ru/perevod-chisel.html
+ * ASCII TABLE C++: http://en.cppreference.com/w/cpp/language/ascii*/
+
+/*Определить оперции для шаблонов
+ * Вынести деление на короткое в отдельную функцию
+ * Написать какие-то модули проверочные
+ * мне жопа
+ * */
+
 #include <limits>
 #include <iostream>
 #include <cmath>
 #include <vector>
 
-template <typename T, size_t Base = std::numeric_limits<T>::max()>
-class BigInteger{
+using namespace std;
+
+template<typename T, size_t NewBase = numeric_limits<T>::max()>
+class BigInteger {
 public:
-    explicit BigInteger<T, Base> (long long value = 0);
+    explicit BigInteger<T, NewBase>(long long value = 0);
 
-    //template <typename T1, size_t NewBase>
-    explicit operator BigInteger<T, Base>();
+    explicit operator BigInteger<T, NewBase>();
 
-    BigInteger<T, Base> (const BigInteger<T, Base> &);
+    BigInteger<T, NewBase>(const BigInteger<T, NewBase> &);
 
-    BigInteger<T, Base> (BigInteger<T, Base> &&);
+    BigInteger<T, NewBase>(BigInteger<T, NewBase> &&);
 
-    BigInteger<T, Base> &operator+=(const BigInteger<T, Base> &);
+    BigInteger<T, NewBase> &operator+=(const BigInteger<T, NewBase> &);
 
-    BigInteger<T, Base>  operator+(const BigInteger<T, Base> &) const;
+    BigInteger<T, NewBase> operator+(const BigInteger<T, NewBase> &) const;
 
-    BigInteger<T, Base>  &operator-=(const BigInteger<T, Base> &);
+    BigInteger<T, NewBase> &operator-=(const BigInteger<T, NewBase> &);
 
-    BigInteger<T, Base>  operator-(const BigInteger<T, Base> &) const;
+    BigInteger<T, NewBase> operator-(const BigInteger<T, NewBase> &) const;
 
-    bool isLess( const BigInteger<T, Base> &);
+    BigInteger<T, NewBase> &operator*=(const BigInteger<T, NewBase> &);
 
-    //size_t Size();
+    BigInteger<T, NewBase> operator*(const BigInteger<T, NewBase> &) const;
 
-    //T operator[](size_t id);
+    BigInteger<T, NewBase> &operator/=(const size_t &);
 
-    std::vector<int> num;
+    void transfer();
+
+    bool isLess(const BigInteger<T, NewBase> &);
+
+    size_t Size();
+
+    bool check();
+
+    T operator[](size_t id);
+
+    string toString();
 private:
+    vector<int> intNumber;
+    vector<T> number;
 
     bool isPos = true;
-    const int POWER = std::numeric_limits<int>::digits10;
-    const int BASE = (int)pow(10, std::numeric_limits<int>::digits10);
 
-    template <typename  U, size_t NewBase>
-    friend std::istream &operator>>(std::istream &, BigInteger<U, NewBase> &);
-    template <typename U, size_t NewBase>
-    friend std::ostream &operator<<(std::ostream &, const BigInteger<U, NewBase> &);
+    int tmpCarry;
+    int amountOfDigits;
+    int bitsForBase;
+    int tSize;
+
+    const int POWER = numeric_limits<int>::digits10;
+    const int BASE = (int) pow(10, numeric_limits<int>::digits10);
+
+    template<typename U, size_t Base>
+    friend istream &operator>>(istream &, BigInteger<U, Base> &);
+
+    template<typename U, size_t Base>
+    friend ostream &operator<<(ostream &, const BigInteger<U, Base> &);
 };
-template <typename  T, size_t Base>
-BigInteger<T, Base>::operator BigInteger<T, Base>() {
+
+template<typename T, size_t NewBase>
+BigInteger<T, NewBase>::operator BigInteger<T, NewBase>() {
+
+//Удали это позже, Конструкто нужен зачем-то, несмотря на то, что при создании сюда даже не заходит
+    bitsForBase = 1;
+    while ((1 << bitsForBase) < NewBase) {
+        ++bitsForBase;
+    }
+
+    amountOfDigits = numeric_limits<T>::digits / bitsForBase;
+    //  bigInt.number.assign(ceil(remainders.size() / (double) amountOfDigits), 0);
+
 
 };
 
@@ -53,69 +94,61 @@ BigInteger<T, NewBase>::BigInteger(long long value) {
     value = llabs(value);
 
     while (value > 0) {
-        num.push_back(value % BASE);
+        intNumber.push_back(value % BASE);
         value /= BASE;
     }
+    if (value != 0)
+        this->transfer();
 }
 
 template<typename T, size_t NewBase>
 BigInteger<T, NewBase>::BigInteger(const BigInteger<T, NewBase> &other) {
-    num = other.num;
+    intNumber = other.intNumber;
     isPos = other.isPos;
 }
+
 template<typename T, size_t NewBase>
-BigInteger<T, NewBase>::BigInteger(BigInteger<T, NewBase> &&bigInt) {
-    num = bigInt.num;
-    isPos = bigInt.isPos;
-    // delete bigInt;
+BigInteger<T, NewBase>::BigInteger(BigInteger<T, NewBase> &&other) {
+    intNumber = other.intNumber;
+    isPos = other.isPos;
 }
 
 template<typename T, size_t NewBase>
-BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator+=(const BigInteger<T, NewBase> &bigInt) {
-    if (isPos == bigInt.isPos) {
+BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator+=(const BigInteger<T, NewBase> &other) {
+    if (isPos == other.isPos) {
         int f = 0;
-        for (int i = 0; f || i < std::max(num.size(), bigInt.num.size()); ++i) {
-            if (i == num.size()) {
-                num.push_back(0);
+        for (int i = 0; f || i < max(intNumber.size(), other.intNumber.size()); ++i) {
+            if (i == intNumber.size()) {
+                intNumber.push_back(0);
             }
 
-            if (i < bigInt.num.size()) {
-                num[i] += f + bigInt.num[i];
+            if (i < other.intNumber.size()) {
+                intNumber[i] += f + other.intNumber[i];
             }
 
-            f = (num[i] >= BASE);
+            f = (intNumber[i] >= BASE);
 
             if (f)
-                num[i] -= BASE;
+                intNumber[i] -= BASE;
         }
     } else {
         isPos ^= true;
-        *this -= bigInt;
+        *this -= other;
         isPos ^= true;
     }
 
-    if (num.back() == 0) {
+    if (intNumber.back() == 0) {
         isPos = true;
     }
+    this->transfer();
     return *this;
 }
 
-
 template<typename T, size_t NewBase>
-BigInteger<T, NewBase> BigInteger<T, NewBase>::operator+(const BigInteger<T, NewBase> &bigInt) const {
-    return BigInteger<T, NewBase>(*this) += bigInt;
+BigInteger<T, NewBase> BigInteger<T, NewBase>::operator+(const BigInteger<T, NewBase> &other) const {
+    return BigInteger<T, NewBase>(*this) += other;
 }
 
-template<typename T, size_t NewBase>
-bool BigInteger<T, NewBase>::isLess(const BigInteger<T, NewBase> &other) {
-    if (num.size() == other.num.size()) {
-        for (int i = 0; i < num.size(); ++i) {
-            if (other.num[i] > num[i]) {
-                return true;
-            }
-        }
-    } else return other.num.size() > num.size();
-}
 template<typename T, size_t NewBase>
 BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator-=(const BigInteger<T, NewBase> &other) {
 
@@ -124,29 +157,29 @@ BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator-=(const BigInteger<T, N
         int ifLess = 1;
 
         if (isLess(other)) {
-            isPos = other.isPos ^ true;
+            isPos = !other.isPos;
             ifLess = -1;
-            int diff = other.num.size() - num.size();
+            int diff = (int) other.intNumber.size() - (int) intNumber.size();
             for (int i = 0; i < diff; ++i) {
-                num.push_back(0);
+                intNumber.push_back(0);
             }
         }
 
-        for (int i = 0; f || i < other.num.size(); ++i) {
-            num[i] *= ifLess;
-            num[i] -= f;
+        for (int i = 0; f || i < other.intNumber.size(); ++i) {
+            intNumber[i] *= ifLess;
+            intNumber[i] -= f;
 
-            if (other.num.size() > i)
-                num[i] -= (other.num[i] * ifLess);
+            if (other.intNumber.size() > i)
+                intNumber[i] -= (other.intNumber[i] * ifLess);
 
-            f = (num[i] < 0);
+            f = (intNumber[i] < 0);
 
             if (f)
-                num[i] += BASE;
+                intNumber[i] += BASE;
         }
 
-        while (num.size() > 1 && num.back() == 0) {
-            num.pop_back();
+        while (intNumber.size() > 1 && intNumber.back() == 0) {
+            intNumber.pop_back();
         }
     } else {
         isPos ^= true;
@@ -154,9 +187,11 @@ BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator-=(const BigInteger<T, N
         isPos ^= true;
     }
 
-    if (num.back() == 0) {
+    if (intNumber.back() == 0) {
         isPos = true;
     }
+
+    this->transfer();
     return *this;
 }
 
@@ -166,38 +201,134 @@ BigInteger<T, NewBase> BigInteger<T, NewBase>::operator-(const BigInteger<T, New
 }
 
 template<typename T, size_t NewBase>
-std::istream &operator>>(std::istream &in, BigInteger<T, NewBase> &other) {
-    std::string number;
-    in >> number;
+BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator*=(const BigInteger<T, NewBase> &other) {
+    vector<int> product(intNumber.size() + other.intNumber.size());
 
-    int until = 0;
-    if (number[0] == '-') {
-        other.isPos = false;
-        until = 1;
-    }
-
-    for (int i = number.length() - 1; i >= until;) {
-        int p = 0;
-        other.num.push_back(0);
-        while (p < other.POWER && i >= until) {
-            int k = number[i] - '0';
-            other.num.back() += k * pow(10, p++);
-            --i;
+    for (int i = 0; i < intNumber.size(); ++i) {
+        int carry = 0;
+        for (int j = 0; j < other.intNumber.size() || carry; ++j) {
+            long long cur =
+                    product[i + j] +
+                    (long long) intNumber[i] * (long long) (j < other.intNumber.size() ? other.intNumber[j] : 0) +
+                    carry;
+            product[i + j] = cur % BASE;
+            carry = int(cur / BASE);
         }
-
     }
+
+//хочу как в джаве ссылку поменять
+    while (product.size() > 1 && !product.back())
+        product.pop_back();
+
+    intNumber = product;
+    isPos = (isPos == other.isPos);
+    this->transfer();
+    return *this;
 }
 
 template<typename T, size_t NewBase>
-std::ostream &operator<<(std::ostream &out, const BigInteger<T, NewBase> &other) {
+BigInteger<T, NewBase> BigInteger<T, NewBase>::operator*(const BigInteger<T, NewBase> &other) const {
+    return BigInteger<T, NewBase>(*this) *= other;
+}
+
+template<typename T, size_t NewBase>
+bool BigInteger<T, NewBase>::isLess(const BigInteger<T, NewBase> &other) {
+    if (intNumber.size() == other.intNumber.size()) {
+        for (int i = 0; i < intNumber.size(); ++i) {
+            if (other.intNumber[i] > intNumber[i]) {
+                return true;
+            }
+        }
+    } else return other.intNumber.size() > intNumber.size();
+}
+
+template<typename T, size_t NewBase>
+istream &operator>>(istream &in, BigInteger<T, NewBase> &bigInt) {
+    string bigNumber;
+    in >> bigNumber;
+
+    int until = 0;
+    if (bigNumber[0] == '-') {
+        bigInt.isPos = false;
+        until = 1;
+    }
+
+    for (int i = (int) bigNumber.length() - 1; i >= until;) {
+        int p = 0;
+        bigInt.intNumber.push_back(0);
+        while (p < bigInt.POWER && i >= until) {
+            int k = bigNumber[i] - '0';
+            bigInt.intNumber.back() += k * pow(10, p++);
+            --i;
+        }
+    }
+
+    bigInt.transfer();
+}
+
+template<typename T, size_t NewBase>
+void BigInteger<T, NewBase>::transfer() {
+    vector<int> tmpIntNum = intNumber;
+    vector<int> remainders;
+
+    while (intNumber.back() != 0) {
+        *this /= NewBase;
+        remainders.push_back(tmpCarry);
+    }
+    tSize = int(remainders.size());
+
+    bitsForBase = 1;
+    while ((1 << bitsForBase) < NewBase) {
+        ++bitsForBase;
+    }
+
+    amountOfDigits = numeric_limits<T>::digits / bitsForBase;
+    number.assign(ceil(remainders.size() / (double) amountOfDigits), 0);
+
+    int j = 0;
+    for (int i = (int) remainders.size() - 1; i >= 0;) {
+        int bitsCnt = 0;
+        while (bitsCnt < amountOfDigits) {
+            int tmp = (remainders[i] << (amountOfDigits - 1 - bitsCnt) * bitsForBase);
+            number[j] |= tmp;
+            ++bitsCnt;
+            --i;
+        }
+        ++j;
+    }
+
+    intNumber = tmpIntNum;
+};
+
+template<typename T, size_t NewBase>
+BigInteger<T, NewBase> &BigInteger<T, NewBase>::operator/=(const size_t &a) {
+    tmpCarry = 0;
+
+    for (auto i = (int) (intNumber.size() - 1); i >= 0; --i) {
+        long long cur = (long long) tmpCarry * (long long) BASE;
+        cur += intNumber[i];
+        intNumber[i] = int(cur / a);
+        tmpCarry = int(cur % a);
+    }
+
+    while (intNumber.size() > 1 && intNumber.back() == 0) {
+        intNumber.pop_back();
+    }
+
+    return *this;
+};
+
+template<typename T, size_t NewBase>
+ostream &operator<<(ostream &out, const BigInteger<T, NewBase> &other) {
     if (!other.isPos) {
         out << '-';
     }
-    out << other.num.back();
-    if (other.num.back() != 0) {
-        for (int i = other.num.size() - 2; i >= 0; --i) {
+
+    out << other.intNumber.back();
+    if (other.intNumber.back() != 0) {
+        for (int i = (int) other.intNumber.size() - 2; i >= 0; --i) {
             long long b = other.BASE;
-            long long tmp = other.num[i];
+            long long tmp = other.intNumber[i];
             while (b > 1) {
                 b /= 10;
                 out << tmp / b;
@@ -207,3 +338,54 @@ std::ostream &operator<<(std::ostream &out, const BigInteger<T, NewBase> &other)
     }
     return out;
 }
+
+template<typename T, size_t NewBase>
+T BigInteger<T, NewBase>::operator[](size_t id) {
+    T tmp = number[ceil(id / (double) amountOfDigits)];
+    return number[ceil(id / (double) amountOfDigits)];
+}
+
+template<typename T, size_t NewBase>
+size_t BigInteger<T, NewBase>::Size() {
+    return tSize;
+}
+
+template<typename T, size_t NewBase>
+bool BigInteger<T, NewBase>::check() {
+    //проверить число можно если оно меньше лонг_макс
+    long long q = 0;
+    for (int i = intNumber.size() - 1; i >= 0; --i) {
+        q += intNumber[i] * pow(BASE, i);
+    }
+    string intInNormBase = to_string(q);
+    transfer();
+    string intIntTBase = "";
+
+    long long d = 0;
+    int gen = (1 << sizeof(T) * 8) - 1;
+    for (int i = 0, p = tSize - 1; i < number.size(); ++i) {
+        for (int k = 1, h = amountOfDigits - 1; p >= 0, k <= amountOfDigits; ++k, --h) {
+            int lSh = sizeof(T) * 8 - amountOfDigits * bitsForBase + (k - 1) * bitsForBase;
+            int rSh = (amountOfDigits - k) * bitsForBase + lSh;
+            int e = (number[i] << lSh)& gen;
+            int f = ( e >> rSh) ;
+
+            d += pow(NewBase, p) * f;
+            --p;
+        }
+    }
+    intIntTBase += to_string(d);
+
+
+return intInNormBase == intIntTBase;
+};
+
+template <typename T, size_t NewBase>
+string BigInteger<T, NewBase>::toString() {
+    long long q = 0;
+    for (int i = intNumber.size() - 1; i >= 0; --i) {
+        long long h = intNumber[i] * pow(BASE, i);
+        q += h;
+    }
+    return to_string(q);
+};
